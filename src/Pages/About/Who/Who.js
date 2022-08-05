b9ea98c66080b0fca8c8c543cca1cad4f7d0b151
Kacper Pełka <kacper.pelka03@gmail.com>
2022-08-05 19:23:45 +0200

    Create About Video on About Page

--- src/Pages/About/Who/Who.js
+++ src/Pages/About/Who/Who.js
@@ -1,8 +1,24 @@
-import Button from '../../../Reusable Components/Button/Button'
 import Logo from '../../../Reusable Components/Logo/Logo'
+import { useState, useRef, useEffect } from 'react'
+
 import './Who.css'
 
 const Who = () => {
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false)
+  let videoDiv = useRef(null)
+
+  document.addEventListener('fullscreenchange', () => {
+    if (!document.fullscreenElement) {
+      setIsVideoPlaying(false)
+    }
+  })
+
+  useEffect(() => {
+    if (isVideoPlaying) {
+      videoDiv.current.requestFullscreen()
+    }
+  }, [isVideoPlaying])
+
   return (
     <section className='who'>
       <div className='whoText'>
@@ -18,7 +34,42 @@ const Who = () => {
           et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
           takimata sanctus est Lorem ipsum dolor
         </p>
-        <Button text='Explore' />
+        <button
+          className='link whoButton'
+          onClick={() => {
+            setIsVideoPlaying(true)
+          }}>
+          Explore{' '}
+          <svg
+            width='24'
+            height='24'
+            xmlns='http://www.w3.org/2000/svg'
+            fillRule='evenodd'
+            clipRule='evenodd'
+            fill='white'>
+            <path d='M23 12l-22 12v-24l22 12zm-21 10.315l18.912-10.315-18.912-10.315v20.63z' />
+          </svg>
+        </button>
+        {isVideoPlaying == true ? (
+          <video
+            className='whoVideo'
+            autoPlay
+            ref={videoDiv}
+            onEnded={() => {
+              setIsVideoPlaying(false)
+            }}
+            onKeyDown={e => {
+              console.log(e.key)
+            }}>
+            <source
+              src={new URL('polsaAbout.mp4', import.meta.url)}
+              type='video/mp4'
+            />
+            <track kind='captions' />
+          </video>
+        ) : (
+          <></>
+        )}
       </div>
       <Logo width={637} height={243} />
     </section>
